fcae12a8e01bc25b0dc2fa8b060dbc836cad4fab
Facundo Ramallo <facu.ramallo@gmail.com>
2024-03-12 17:50:12 -0300

    clase java 12 3 2024

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+        public static void main(String[] args) {
+            imprimir();
+        }
+    public static void imprimir(){
+        System.out.printf("Hello and welcome!   ");
+    }
+}
+
